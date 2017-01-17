33c47e655de70ce01967c02ed6b7ae6f14470f31
Chinseok Lee <allieuslee@gmail.com>
2017-01-17 16:37:42 +0900

    accept connection

--- blog/consumers.py
+++ blog/consumers.py
@@ -7,6 +7,8 @@ from channels.auth import http_session_user, channel_session_user, channel_sessi
 
 @channel_session_user_from_http
 def ws_add(message):
+    message.reply_channel.send({'accept': True})  # accept connection
+
     room = message.content['path'].strip("/")
     message.channel_session['room'] = room
     Group('chat-{}'.format(room)).add(message.reply_channel)
